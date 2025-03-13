bab8011dbe5d04b0d4fd77e529a9303f39f85fb8
fayedsazli <fayedsazli07@gmail.com>
2025-03-13 15:15:42 +0800

    feat: Banner CTA add

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,21 @@
 import Navbar from "../layout/Navbar";
-import HomeTopBanner from "../layout/HomeTopBanner";
+import BackgroundBanner from "../layout/HomeTopBanner";
 import CardSection from "../layout/CardSection";
+import BannerText from "../common/BannerText";
+import BannerCTA from "../common/BannerCTA";
+
 
 const Home = () => {
   return (
     <div className="d-flex flex-column gap-3 mb-3 justify-content-center">
       <Navbar />
-      <HomeTopBanner />
+      <BackgroundBanner img="background-imgae.jpeg">
+        <BannerText />
+      </BackgroundBanner>
       <CardSection />
+      <BackgroundBanner img="background2.jpeg">
+      <BannerCTA />
+      </BackgroundBanner>
     </div>
   );
 };
